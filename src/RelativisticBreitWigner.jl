283f072fe3b65c97f0d7a2d68094afa7bab4ef58
subrajit-puhan <subrajitpuhan14@gmail.com>
2025-10-31 14:07:30 +0530

    Changed the type of inputs and promoted them

--- src/RelativisticBreitWigner.jl
+++ src/RelativisticBreitWigner.jl
@@ -1,24 +1,28 @@
-function check_bw_params( M::T, Γ::T) where {T <: Real}
-    M > zero(T) || error("M must be positive")
-    Γ > zero(T) || error("Γ must be positive")
-end
-
 struct RelativisticBreitWigner{T <: Real} <: ContinuousUnivariateDistribution 
     M::T
     Γ::T
 
     function RelativisticBreitWigner(M::T, Γ::T) where {T <: Real}
-        check_bw_params(M, Γ)
+        M > zero(T) || error("M must be positive")
+        Γ > zero(T) || error("Γ must be positive")
         new{T}(M, Γ)
     end
 end 
 
+
+RelativisticBreitWigner(M::Real , Γ::Real) = RelativisticBreitWigner(promote(M, Γ)...)
+RelativisticBreitWigner(M::Integer, Γ::Integer) = RelativisticBreitWigner(float(M), float(Γ))
+
 function Distributions.pdf(r::RelativisticBreitWigner{T}, x::Real) where {T <: Real}
-    M,Γ = r.M, r.Γ
-    γ = sqrt(M^2 * (M^2 + Γ^2))
-    k = (2*sqrt(2) * M * Γ * γ)/(π * sqrt(M^2 + γ))
-    dom = (x^2 - M^2)^2 + M^2 * Γ^2
-    return k/dom
+    if x < zero(T)
+        zero(T)
+    else
+        M, Γ = r.M, r.Γ
+        γ = sqrt(M^2 * (M^2 + Γ^2))
+        k = (T(2)*sqrt(T(2)) * M * Γ * γ)/(π * sqrt(M^2 + γ))
+        dom = (x^2 - M^2)^2 + (M^2 * Γ^2)
+        k/dom
+    end
 end
 
 function Distributions.logpdf(r::RelativisticBreitWigner{T}, x::Real) where {T <: Real}
@@ -26,12 +30,17 @@ function Distributions.logpdf(r::RelativisticBreitWigner{T}, x::Real) where {T <
 end
 
 function Distributions.cdf(r::RelativisticBreitWigner{T}, x::Real) where {T <: Real}
-    let ρ = r.M/r.Γ, C =  1/π * √(2 / (1 + √ (1 + 1/ρ²) ))
-        z1 = sqrt(-1 + im / ρ)
-        z2 = sqrt(-ρ * (ρ + im))
-        term = z1 * atan(x / z2)
-        result = 2 * C * imag(term)
-        return min(result, one(T))
+    if x < zero(T)
+        zero(T)
+    else
+        let ρ = r.M/r.Γ, two = T(2)
+            C =  1/T(π) * √(two / (1 + √ (1 + 1/ρ^2) ))
+            z1 = sqrt(-1 + im / ρ)
+            z2 = sqrt(-ρ * (ρ + im))
+            term = z1 * atan(x / z2)
+            result = abs(two * C * imag(term))
+            return min(result, one(T))
+        end
     end
 end
 
@@ -48,8 +57,6 @@ mode(r::RelativisticBreitWigner) = r.M
 var(r::RelativisticBreitWigner{T}) where {T<:Real} = T(NaN)
 skewness(r::RelativisticBreitWigner{T}) where {T<:Real} = T(NaN)
 kurtosis(r::RelativisticBreitWigner{T}) where {T<:Real} = T(NaN)
-
-entropy(r::RelativisticBreitWigner) = log4π + log(r.Γ)
 
 Distributions.minimum(r::RelativisticBreitWigner{T}) where {T <: Real} = T(-Inf)
 Distributions.maximum(r::RelativisticBreitWigner{T}) where {T <: Real} = T(Inf)
